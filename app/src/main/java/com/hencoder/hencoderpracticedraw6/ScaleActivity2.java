bf86520370881aabb71b4b620b9516e5f86e2e3c
oldhigh <844706026@qq.com>
2018-06-26 09:51:15 +0800

    学习属性动画，以及测试一个扩散的动画 ，点击进行跳转Activity

--- app/src/main/java/com/hencoder/hencoderpracticedraw6/ScaleActivity2.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw6/ScaleActivity2.java
@@ -1,8 +1,10 @@
 package com.hencoder.hencoderpracticedraw6;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.TextView;
 
 /**
@@ -18,5 +20,14 @@ public class ScaleActivity2 extends AppCompatActivity {
         view.setText("333333");
         view.setTextSize(33);
         setContentView(view);
+
+
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext() , MainActivity.class));
+                finish();
+            }
+        });
     }
 }
